80d3d94073c60457ed8806316a0e25a4c86e9980
apeterlein <11824265+apeterlein@users.noreply.github.com>
2023-07-01 16:50:23 -0500

    v 2.1

--- main.js
+++ main.js
@@ -5,18 +5,38 @@ $(document).ready(function () {
 	let filename = "";
 	const reader = new FileReader();
 	reader.onload = function (evt) {
-		const zip = new PizZip(evt.target.result);
-		const xml = $.parseXML(zip.files["word/document.xml"].asText())
-		const lines = parseInput(xml);
-		const doc = genOutput(lines);
-		saveOutput(doc, filename);
+		let zip, xml, lines, doc;
+		try { zip = new PizZip(evt.target.result); }
+		catch {
+			logError("I couldn't extract the contents of that file", "Try again on a different browser", error);
+			return;
+		}
+		try { xml = $.parseXML(zip.files["word/document.xml"].asText()); }
+		catch (error) {
+			logError("MS Word has generated some yucky XML that I couldn't parse", "Try removing any links or headers from the document", error);
+			return;
+		}
+		try {
+			lines = parseInput(xml);
+			doc = genOutput(lines);
+		}
+		catch (error) {
+			logError("I ran into a problem formatting text", "Try removing any links or headers from the document", error);
+			return;
+		}
+		try { saveOutput(doc, filename); }
+		catch (error) { logError("I couldn't save the output file", "Try again on a different browser", error); }
 	};
 	$("#file").change(function () {
+		$("#error").html("");
+		$("#details").html("");
 		const file = document.getElementById("file").files[0];
 		if (file && /\.docx$/g.test(file.name)) {
 			filename = file.name.substring(0, file.name.length - 5);
-			reader.readAsArrayBuffer(file, "UTF-8");
+			try { reader.readAsArrayBuffer(file, "UTF-8"); }
+			catch (error) { logError("I couldn't read content from that file", "Try again using a different browser", error); }
 		}
+		else { logError("only .docx files are supported", "Try saving as a .docx", "{ name: " + file.name + ", type: " + file.type + " }"); }
 		$("#file").val("");
 	});
 });
@@ -29,11 +49,14 @@ function parseInput(xml) {
 		if (tag === "w:p" || tag === "w:br") {
 			str = str.trim();
 			if (/^\d\d:\d\d:\d\d [a-zA-Z\d ]+/g.test(str)) {
-				row++;
-				lines[row] = {};
-				lines[row]["Time"] = str.substring(0, 8);
-				lines[row]["Speaker"] = str.substring(9, str.length);
-				lines[row]["Text"] = [];
+				const speaker = str.substring(9, str.length);
+				if (!$("#cons").is(':checked') || !(lines[row] && speaker === lines[row]["Speaker"])) {
+					row++;
+					lines[row] = {};
+					lines[row]["Time"] = str.substring(0, 8);
+					lines[row]["Speaker"] = speaker;
+					lines[row]["Text"] = [];
+				}
 			}
 			else if (str.length !== 0 && lines[row] !== undefined) {
 				lines[row]["Text"].push(str);
@@ -92,6 +115,10 @@ function genOutput(lines) {
 	for (let i = 0; i < lines.length; i++) {
 		let cont = []
 		for (let j = 0; j < lines[i]["Text"].length; j++) {
+			if (cont.length > 0) {
+				if ($("#brks").is(':checked')) cont.push(new docx.TextRun({ break: 1 }));
+				else cont.push(new docx.TextRun({ text: " " }));
+			}
 			cont.push(new docx.TextRun({ text: lines[i]["Text"][j] }));
 		}
 		let row = new docx.TableRow({
@@ -139,4 +166,8 @@ function genOutput(lines) {
 }
 function saveOutput(doc, filename) {
 	docx.Packer.toBlob(doc).then(function (blob) { saveAs(blob, filename + "_out.docx"); });
+}
+function logError(txt, hlp, error) {
+	$("#error").html("Sorry, " + txt + " :(");
+	$("#details").html(hlp + ". If you need help, send this junk to Adam: <span class=\"fw-light fst-italic\">" + error + "</span>");
 }
